5b0bed32d39d27ba0312b8e3f0e0b2b0108b708d
Pritam Saha <averagestudent70@gmail.com>
2024-09-26 19:17:51 +0530

    added functionality for debug and release modes to template file

--- template.cpp
+++ template.cpp
@@ -1,3 +1,14 @@
+/**
+ * Advent of Code Day 1.1
+ * date solved: 17 sept 2023
+ * 
+ * Pass the name of the input file as an argument to the program. 
+ * Default file name is `input`.
+ * 
+ * If `NDEBUG` is not set, default file name is `test`, and the program 
+ * also runs the test function(s).
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,22 +21,28 @@ using std::vector;
 
 void test_()
 {
-    assert();
+    assert(true);
 }
 
 int main(int argc, const char *argv[])
 {
+    string default_filename = "input";
     #ifndef NDEBUG
-    test_();
-    return 0;
+        default_filename = "test";
     #endif
 
     // argc denotes the number of arguments passed +1 (it includes the program name)
-    const string filename = (argc > 1 ? argv[1] : "input");
+    string filename = (argc > 1 ? argv[1] : default_filename);
+
     std::ifstream in(filename);
     if(!in.is_open()) {
-        std::cerr << "Couldn't open input file `" + filename << "`. Check if you are present"
-                    " in the directory in which the input file is present\n";
+        std::cerr << "Couldn't open input file `" + filename << "`.\n";
         return -1;
     }
+
+    #ifndef NDEBUG
+        test_();
+    #endif   
+
+    return 0;
 }
